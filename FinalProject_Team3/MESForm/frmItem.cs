d284c9f0a6c217bf24e0956e602c3c769989c860
wnendend12@naver.com <wnendend12@naver.com>
2021-01-19 16:01:05 +0900

    dd

---
+++ FinalProject_Team3/MESForm/frmItem.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MESForm
+{
+    public partial class frmItem : Form
+    {
+        public frmItem()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnReg_Click(object sender, EventArgs e)
+        {
+            frmItemPopUp frm = new frmItemPopUp();
+            
+            frm.ShowDialog();
+        }
+    }
+}
