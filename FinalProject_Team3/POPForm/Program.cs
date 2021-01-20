14483ceca864bf7a6a5f183baa41ddc168aba395
상진 오 <azan03178@naver.com>
2021-01-20 16:56:46 +0900

    POP

--- FinalProject_Team3/POPForm/Program.cs
+++ FinalProject_Team3/POPForm/Program.cs
@@ -16,7 +16,7 @@ namespace POPForm
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmPOP());
         }
     }
 }
---
+++ FinalProject_Team3/POPForm/frmPOP.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace POPForm
+{
+    public partial class frmPOP : Form
+    {
+        public frmPOP()
+        {
+            InitializeComponent();
+        }
+
+        private void label13_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label12_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label14_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label11_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
