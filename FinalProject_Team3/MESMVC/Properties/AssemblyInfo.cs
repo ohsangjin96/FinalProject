4706e069087b98e548c25ea8e33b0d5f04e77d7f
Jaedeok Yu <qkfkadmf@naver.com>
2021-01-21 16:04:09 +0900

    MVC화면 1차 구성

--- FinalProject_Team3/MESMVC/Properties/AssemblyInfo.cs
+++ FinalProject_Team3/MESMVC/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 이 프로젝트가 COM에 노출되는 경우 다음 GUID는 typelib의 ID를 나타냅니다.
-[assembly: Guid("dea205cb-a22a-4b41-abbb-590664f802fe")]
+[assembly: Guid("ecc865b5-e960-43b4-ba05-c364523b1787")]
 
 // 어셈블리의 버전 정보는 다음 네 가지 값으로 구성됩니다.:
 //
