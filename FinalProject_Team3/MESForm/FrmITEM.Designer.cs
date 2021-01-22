ad9d365d7835242cd1be7e31fbe82896fbcfc03e
wnendend12@naver.com <wnendend12@naver.com>
2021-01-23 00:30:27 +0900

    품목상속폼

---
+++ FinalProject_Team3/MESForm/FrmITEM.Designer.cs
@@ -0,0 +1,52 @@
+﻿namespace MESForm
+{
+    partial class FrmITEM
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlSelect.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnInquiry
+            // 
+            this.btnInquiry.FlatAppearance.BorderColor = System.Drawing.Color.SteelBlue;
+            // 
+            // FrmITEM
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            this.ClientSize = new System.Drawing.Size(1250, 700);
+            this.Name = "FrmITEM";
+            this.Load += new System.EventHandler(this.FrmITEM_Load);
+            this.pnlSelect.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+    }
+}
---
+++ FinalProject_Team3/MESForm/FrmITEM.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MESForm
+{
+    public partial class FrmITEM : MESForm.BaseForms.frmBaseLists
+    {
+        public FrmITEM()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmITEM_Load(object sender, EventArgs e)
+        {
+            //로드
+        }
+    }
+}
