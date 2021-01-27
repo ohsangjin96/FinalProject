073ef8a8b56f3aa7a3ea50d292eb8d383d65a4fe
wnendend12@naver.com <wnendend12@naver.com>
2021-01-28 01:12:56 +0900

    bom ing~

--- FinalProject_Team3/MESForm/FrmBOM.Designer.cs
+++ FinalProject_Team3/MESForm/FrmBOM.Designer.cs
@@ -28,9 +28,270 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle7 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle9 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle8 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.btnReg = new MESForm.CustomControls.custButtonControl();
+            this.btnUpdate = new MESForm.CustomControls.custButtonControl();
+            this.btnDelete = new MESForm.CustomControls.custButtonControl();
+            this.btnExcel = new MESForm.CustomControls.custButtonControl();
+            this.btnRefresh = new MESForm.CustomControls.custButtonControl();
+            this.cboItemType = new System.Windows.Forms.ComboBox();
+            this.lblFactoryGrade = new System.Windows.Forms.Label();
+            this.txtItemName = new System.Windows.Forms.TextBox();
+            this.lblFactoryCode = new System.Windows.Forms.Label();
+            this.dgvBom = new MESForm.CustomControls.custDataGridViewControl();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cboUseYN = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pnlSelect.SuspendLayout();
+            this.pnlItem.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBom)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlSelect
+            // 
+            this.pnlSelect.Controls.Add(this.cboUseYN);
+            this.pnlSelect.Controls.Add(this.label1);
+            this.pnlSelect.Controls.Add(this.cboItemType);
+            this.pnlSelect.Controls.Add(this.lblFactoryGrade);
+            this.pnlSelect.Controls.Add(this.txtItemName);
+            this.pnlSelect.Controls.Add(this.lblFactoryCode);
+            this.pnlSelect.Size = new System.Drawing.Size(1226, 74);
+            this.pnlSelect.Controls.SetChildIndex(this.btnInquiry, 0);
+            this.pnlSelect.Controls.SetChildIndex(this.lblFactoryCode, 0);
+            this.pnlSelect.Controls.SetChildIndex(this.txtItemName, 0);
+            this.pnlSelect.Controls.SetChildIndex(this.lblFactoryGrade, 0);
+            this.pnlSelect.Controls.SetChildIndex(this.cboItemType, 0);
+            this.pnlSelect.Controls.SetChildIndex(this.label1, 0);
+            this.pnlSelect.Controls.SetChildIndex(this.cboUseYN, 0);
+            // 
+            // btnInquiry
+            // 
+            this.btnInquiry.FlatAppearance.BorderColor = System.Drawing.Color.SteelBlue;
+            this.btnInquiry.Location = new System.Drawing.Point(1154, 28);
+            // 
+            // lblFormName1
+            // 
+            this.lblFormName1.Location = new System.Drawing.Point(12, 114);
+            this.lblFormName1.Size = new System.Drawing.Size(51, 21);
+            this.lblFormName1.Text = "BOM";
+            // 
+            // pnlItem
+            // 
+            this.pnlItem.Controls.Add(this.dgvBom);
+            this.pnlItem.Location = new System.Drawing.Point(12, 141);
+            this.pnlItem.Size = new System.Drawing.Size(1226, 581);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnReg);
+            this.panel1.Controls.Add(this.btnUpdate);
+            this.panel1.Controls.Add(this.btnDelete);
+            this.panel1.Controls.Add(this.btnExcel);
+            this.panel1.Controls.Add(this.btnRefresh);
+            this.panel1.Location = new System.Drawing.Point(670, 94);
+            // 
+            // btnReg
+            // 
+            this.btnReg.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.btnReg.BackColor = System.Drawing.Color.LightSlateGray;
+            this.btnReg.FlatAppearance.BorderColor = System.Drawing.Color.LightSteelBlue;
+            this.btnReg.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnReg.Font = new System.Drawing.Font("나눔스퀘어OTF", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnReg.ForeColor = System.Drawing.Color.Black;
+            this.btnReg.Location = new System.Drawing.Point(122, 3);
+            this.btnReg.Name = "btnReg";
+            this.btnReg.Size = new System.Drawing.Size(83, 32);
+            this.btnReg.TabIndex = 14;
+            this.btnReg.Text = "등록";
+            this.btnReg.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnReg.UseVisualStyleBackColor = false;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.btnUpdate.BackColor = System.Drawing.Color.LightSlateGray;
+            this.btnUpdate.FlatAppearance.BorderColor = System.Drawing.Color.LightSteelBlue;
+            this.btnUpdate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnUpdate.Font = new System.Drawing.Font("나눔스퀘어OTF", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnUpdate.ForeColor = System.Drawing.Color.Black;
+            this.btnUpdate.Location = new System.Drawing.Point(211, 3);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(83, 32);
+            this.btnUpdate.TabIndex = 13;
+            this.btnUpdate.Text = "수정";
+            this.btnUpdate.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnUpdate.UseVisualStyleBackColor = false;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.btnDelete.BackColor = System.Drawing.Color.LightSlateGray;
+            this.btnDelete.FlatAppearance.BorderColor = System.Drawing.Color.LightSteelBlue;
+            this.btnDelete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDelete.Font = new System.Drawing.Font("나눔스퀘어OTF", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnDelete.ForeColor = System.Drawing.Color.Black;
+            this.btnDelete.Location = new System.Drawing.Point(300, 3);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(83, 32);
+            this.btnDelete.TabIndex = 12;
+            this.btnDelete.Text = "삭제";
+            this.btnDelete.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnDelete.UseVisualStyleBackColor = false;
+            // 
+            // btnExcel
+            // 
+            this.btnExcel.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.btnExcel.BackColor = System.Drawing.Color.LightSlateGray;
+            this.btnExcel.FlatAppearance.BorderColor = System.Drawing.Color.LightSteelBlue;
+            this.btnExcel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExcel.Font = new System.Drawing.Font("나눔스퀘어OTF", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnExcel.ForeColor = System.Drawing.Color.Black;
+            this.btnExcel.Location = new System.Drawing.Point(391, 3);
+            this.btnExcel.Name = "btnExcel";
+            this.btnExcel.Size = new System.Drawing.Size(83, 32);
+            this.btnExcel.TabIndex = 11;
+            this.btnExcel.Text = "엑셀";
+            this.btnExcel.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnExcel.UseVisualStyleBackColor = false;
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.btnRefresh.BackColor = System.Drawing.Color.LightSlateGray;
+            this.btnRefresh.FlatAppearance.BorderColor = System.Drawing.Color.LightSteelBlue;
+            this.btnRefresh.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRefresh.Font = new System.Drawing.Font("나눔스퀘어OTF", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnRefresh.ForeColor = System.Drawing.Color.Black;
+            this.btnRefresh.Location = new System.Drawing.Point(480, 3);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(83, 32);
+            this.btnRefresh.TabIndex = 10;
+            this.btnRefresh.Text = "새로고침";
+            this.btnRefresh.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnRefresh.UseVisualStyleBackColor = false;
+            // 
+            // cboItemType
+            // 
+            this.cboItemType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboItemType.FormattingEnabled = true;
+            this.cboItemType.Location = new System.Drawing.Point(499, 27);
+            this.cboItemType.Name = "cboItemType";
+            this.cboItemType.Size = new System.Drawing.Size(191, 22);
+            this.cboItemType.TabIndex = 33;
+            // 
+            // lblFactoryGrade
+            // 
+            this.lblFactoryGrade.AutoSize = true;
+            this.lblFactoryGrade.Font = new System.Drawing.Font("나눔스퀘어OTF Bold", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.lblFactoryGrade.Location = new System.Drawing.Point(380, 30);
+            this.lblFactoryGrade.Name = "lblFactoryGrade";
+            this.lblFactoryGrade.Size = new System.Drawing.Size(55, 14);
+            this.lblFactoryGrade.TabIndex = 32;
+            this.lblFactoryGrade.Text = "품목유형";
+            // 
+            // txtItemName
+            // 
+            this.txtItemName.Location = new System.Drawing.Point(125, 27);
+            this.txtItemName.Name = "txtItemName";
+            this.txtItemName.Size = new System.Drawing.Size(191, 22);
+            this.txtItemName.TabIndex = 31;
+            // 
+            // lblFactoryCode
+            // 
+            this.lblFactoryCode.AutoSize = true;
+            this.lblFactoryCode.Font = new System.Drawing.Font("나눔스퀘어OTF Bold", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.lblFactoryCode.Location = new System.Drawing.Point(30, 30);
+            this.lblFactoryCode.Name = "lblFactoryCode";
+            this.lblFactoryCode.Size = new System.Drawing.Size(31, 14);
+            this.lblFactoryCode.TabIndex = 30;
+            this.lblFactoryCode.Text = "품목";
+            // 
+            // dgvBom
+            // 
+            dataGridViewCellStyle7.BackColor = System.Drawing.Color.White;
+            this.dgvBom.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle7;
+            this.dgvBom.BackgroundColor = System.Drawing.Color.White;
+            this.dgvBom.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dgvBom.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvBom.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1});
+            dataGridViewCellStyle9.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(248)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle9.Font = new System.Drawing.Font("나눔스퀘어OTF", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            dataGridViewCellStyle9.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle9.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle9.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle9.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvBom.DefaultCellStyle = dataGridViewCellStyle9;
+            this.dgvBom.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvBom.Font = new System.Drawing.Font("나눔스퀘어OTF", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.dgvBom.Location = new System.Drawing.Point(0, 0);
+            this.dgvBom.Name = "dgvBom";
+            this.dgvBom.RowTemplate.Height = 23;
+            this.dgvBom.Size = new System.Drawing.Size(1226, 581);
+            this.dgvBom.TabIndex = 0;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.DataPropertyName = "no";
+            dataGridViewCellStyle8.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.dataGridViewTextBoxColumn1.DefaultCellStyle = dataGridViewCellStyle8;
+            this.dataGridViewTextBoxColumn1.HeaderText = "No";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            this.dataGridViewTextBoxColumn1.ReadOnly = true;
+            this.dataGridViewTextBoxColumn1.Width = 50;
+            // 
+            // cboUseYN
+            // 
+            this.cboUseYN.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboUseYN.FormattingEnabled = true;
+            this.cboUseYN.Location = new System.Drawing.Point(873, 27);
+            this.cboUseYN.Name = "cboUseYN";
+            this.cboUseYN.Size = new System.Drawing.Size(191, 22);
+            this.cboUseYN.TabIndex = 35;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("나눔스퀘어OTF Bold", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(754, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 14);
+            this.label1.TabIndex = 34;
+            this.label1.Text = "사용유무";
+            // 
+            // FrmBOM
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            this.ClientSize = new System.Drawing.Size(1250, 700);
+            this.Name = "FrmBOM";
+            this.pnlSelect.ResumeLayout(false);
+            this.pnlSelect.PerformLayout();
+            this.pnlItem.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBom)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private CustomControls.custButtonControl btnReg;
+        private CustomControls.custButtonControl btnUpdate;
+        private CustomControls.custButtonControl btnDelete;
+        private CustomControls.custButtonControl btnExcel;
+        private CustomControls.custButtonControl btnRefresh;
+        private System.Windows.Forms.ComboBox cboItemType;
+        private System.Windows.Forms.Label lblFactoryGrade;
+        private System.Windows.Forms.TextBox txtItemName;
+        private System.Windows.Forms.Label lblFactoryCode;
+        private CustomControls.custDataGridViewControl dgvBom;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.ComboBox cboUseYN;
+        private System.Windows.Forms.Label label1;
     }
 }
