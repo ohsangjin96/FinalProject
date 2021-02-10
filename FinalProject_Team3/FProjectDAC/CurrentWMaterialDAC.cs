e117b394b4fe618458a916aca9a4857415852725
Jaedeok Yu <qkfkadmf@naver.com>
2021-02-10 13:41:41 +0900

    입고처리(수정), 자재입고현황, 자재재고현황 (21-02-10)

---
+++ FinalProject_Team3/FProjectDAC/CurrentWMaterialDAC.cs
@@ -0,0 +1,51 @@
+﻿using FProjectVO;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FProjectDAC
+{
+    public class CurrentWMaterialDAC : ConnectionAccess, IDisposable
+    {
+        string strConn;
+        SqlConnection conn;
+
+        public CurrentWMaterialDAC()
+        {
+            strConn = this.ConnectionString;
+            EncrytLibrary.AES aes = new EncrytLibrary.AES();
+            strConn = aes.AESDecrypt256(strConn);
+            conn = new SqlConnection(strConn);
+            conn.Open();
+        }
+
+        public void Dispose()
+        {
+            conn.Close();
+        }
+
+        public List<CurrentWMaterialVO> GetCurrentWMaterialList()
+        {
+            using (SqlCommand cmd = new SqlCommand())
+            {
+                cmd.Connection = conn;
+                cmd.CommandText = @"select ROW_NUMBER() OVER (Order by Reorder_His_No desc) RowNo, Reorder_His_No,
+                                           Reorder_InDate, ITEM_WareHouse_IN, I.ITEM_Code, ITEM_Name,
+                                           ITEM_Standard, ITEM_Unit, RH.Reorder_InAmount, RH.Reorder_Cancel, 
+                                           Reorder_Balance, Com_Name, ITEM_Delivery_Company
+                                    from Reorder_His RH join Reorder R on RH.Reorder_Number = R.Reorder_Number
+					                                    join ITEM I on RH.ITEM_Code = I.ITEM_Code
+                                    order by Reorder_His_No desc";
+
+                SqlDataReader reader = cmd.ExecuteReader();
+                List<CurrentWMaterialVO> list = Helper.DataReaderMapToList<CurrentWMaterialVO>(reader);
+
+                return list;
+            }
+        }
+    }
+}
---
+++ FinalProject_Team3/FProjectDAC/CurrentWStockDAC.cs
@@ -0,0 +1,49 @@
+﻿using FProjectVO;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FProjectDAC
+{
+    public class CurrentWStockDAC : ConnectionAccess, IDisposable
+    {
+        string strConn;
+        SqlConnection conn;
+        public CurrentWStockDAC()
+        {
+            strConn = this.ConnectionString;
+            EncrytLibrary.AES aes = new EncrytLibrary.AES();
+            strConn = aes.AESDecrypt256(strConn);
+            conn = new SqlConnection(strConn);
+            conn.Open();
+        }
+
+        public void Dispose()
+        {
+            conn.Close();
+        }
+
+        public List<CurrentWStockVO> GetCurrentWStockList()
+        {
+            using(SqlCommand cmd = new SqlCommand())
+            {
+                cmd.Connection = conn;
+                cmd.CommandText = @"select ROW_NUMBER() OVER (Order by MAX(Warehouse_ID) desc) RowNo, Factory_Code,
+                                           ITEM_WareHouse_IN, W.ITEM_Code, ITEM_Name, ITEM_Type, ITEM_Standard,
+	                                       SUM(Warehouse_StockQty) Warehouse_StockQty, ITEM_Unit, Reorder_Note
+                                    from Warehouse W join Reorder R on W.Reorder_Number = R.Reorder_Number
+				                                     join ITEM I on W.ITEM_Code = I.ITEM_Code
+                                    group by Factory_Code, ITEM_WareHouse_IN, W.ITEM_Code, ITEM_Name,
+                                             ITEM_Type, ITEM_Standard, ITEM_Unit, Reorder_Note";
+
+                SqlDataReader reader = cmd.ExecuteReader();
+                List<CurrentWStockVO> list = Helper.DataReaderMapToList<CurrentWStockVO>(reader);
+
+                return list;
+            }
+        }
+    }
+}
